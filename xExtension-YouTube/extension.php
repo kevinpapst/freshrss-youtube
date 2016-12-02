1a7252f90e7444251568537f60ca7d30f2cbab51
Kevin Papst <kpapst@gmx.net>
2016-12-02 20:36:03 +0100

    fail safe config loading, added phpdoc

--- xExtension-YouTube/extension.php
+++ xExtension-YouTube/extension.php
@@ -1,21 +1,43 @@
 <?php
 
+/**
+ * Class YouTubeExtension
+ *
+ * Latest version can be found at https://github.com/kevinpapst/freshrss-youtube
+ *
+ * @author Kevin Papst
+ */
 class YouTubeExtension extends Minz_Extension
 {
+    /**
+     * Video player width
+     * @var int
+     */
     public static $width = 560;
+    /**
+     * Video player height
+     * @var int
+     */
     public static $height = 315;
 
+    /**
+     * Initialize this extension
+     */
     public function init()
     {
-        $this->registerHook(
-            'entry_before_display',
-            array($this, 'embedYouTubeVideo')
-        );
-        $this->loadConfigValues();
+        $this->registerHook('entry_before_display', array($this, 'embedYouTubeVideo'));
+        $this->registerTranslates();
     }
 
+    /**
+     * Initializes the extension configuration, if the user context is available.
+     */
     protected function loadConfigValues()
     {
+        if (null === FreshRSS_Context || null === FreshRSS_Context::$user_conf) {
+            return;
+        }
+
         if (FreshRSS_Context::$user_conf->yt_player_width != '') {
             self::$width = FreshRSS_Context::$user_conf->yt_player_width;
         }
@@ -25,6 +47,8 @@ class YouTubeExtension extends Minz_Extension
     }
 
     /**
+     * Inserts the YouTube video iframe into the content of an entry, if the entries link points to a YouTube watch URL.
+     *
      * @param FreshRSS_Entry $entry
      * @return mixed
      */
@@ -41,17 +65,20 @@ class YouTubeExtension extends Minz_Extension
         $url = str_replace('http://', 'https://', $url);
 
         $entry->_content(
-            '<iframe width="'.self::$width.'" height="'.self::$height.'" src="'.$url.'" frameborder="0" allowfullscreen></iframe>' .
+            '<iframe width="' . self::$width . '" height="' . self::$height . '" src="' . $url .
+            '" frameborder="0" allowfullscreen></iframe>' .
             $entry->content()
         );
 
         return $entry;
     }
 
+    /**
+     * Saves the user settings for this extension.
+     */
     public function handleConfigureAction()
     {
         $this->loadConfigValues();
-        $this->registerTranslates();
 
         if (Minz_Request::isPost()) {
             FreshRSS_Context::$user_conf->yt_player_height = (int)Minz_Request::param('yt_height', '');
