f5cf55d5074e60d3c59c6568bcc4cd71d0f3a891
Qwerty <champlywood@free.fr>
2016-12-04 20:10:37 +0100

    Create ext.php (#3)
    create french translation

---
+++ xExtension-YouTube/i18n/fr/ext.php
@@ -0,0 +1,9 @@
+<?php
+return 
+  array(	
+  'yt_videos' => array(		
+    'height' => 'Hauteur du lecteur',        
+    'width' => 'Largeur du lecteur',        
+    'updates' => 'Vous pouvez trouver la dernière mise à jour de l’extension sur ',	
+  ),
+);
