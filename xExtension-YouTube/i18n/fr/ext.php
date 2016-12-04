886899f73e25c60739045eba939a91c608512ffb
Qwerty <champlywood@free.fr>
2016-12-04 20:00:47 +0100

    Update ext.php

--- xExtension-YouTube/i18n/fr/ext.php
+++ xExtension-YouTube/i18n/fr/ext.php
@@ -1,1 +1,9 @@
-<?phpreturn array(	'yt_videos' => array(		'height' => 'Hauteur du lecteur',        'width' => 'Largeur du lecteur',        'updates' => 'Vous pouvez trouver la dernière mise à jour de l’extension sur ',	),);
+<?php
+return 
+  array(	
+  'yt_videos' => array(		
+    'height' => 'Hauteur du lecteur',        
+    'width' => 'Largeur du lecteur',        
+    'updates' => 'Vous pouvez trouver la dernière mise à jour de l’extension sur ',	
+  ),
+);
