8d0ac4f0a6f5d2915f7c0c6e780586195608a772
Julien-Pierre Avérous <j.averous@sourcemac.com>
2020-01-16 21:14:17 +0100

    It seems that 'flux' is more used than 'fil' to translate feed.

--- xExtension-YouTube/i18n/fr/ext.php
+++ xExtension-YouTube/i18n/fr/ext.php
@@ -5,7 +5,7 @@ return array(
 		'height' => 'Hauteur du lecteur',
 		'width' => 'Largeur du lecteur',
 		'updates' => 'Vous pouvez trouver la dernière mise à jour de l’extension sur ',
-		'show_content' => 'Afficher le contenu du fil',
+		'show_content' => 'Afficher le contenu du flux',
 		'use_nocookie' => 'Use the cookie-free domain www.youtube-nocookie.com', // @todo translate
 	),
 );
