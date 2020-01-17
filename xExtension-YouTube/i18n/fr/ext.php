61e7b1043f06122376692b5d1c575019a23e230a
Julien-Pierre Avérous <j.averous@sourcemac.com>
2020-01-17 10:27:39 +0100

    [Code-Review from @marienfressinaud] Translate text in French.

--- xExtension-YouTube/i18n/fr/ext.php
+++ xExtension-YouTube/i18n/fr/ext.php
@@ -6,6 +6,6 @@ return array(
 		'width' => 'Largeur du lecteur',
 		'updates' => 'Vous pouvez trouver la dernière mise à jour de l’extension sur ',
 		'show_content' => 'Afficher le contenu du flux',
-		'use_nocookie' => 'Use the cookie-free domain www.youtube-nocookie.com', // @todo translate
+		'use_nocookie' => 'Utiliser le domaine www.youtube-nocookie.com pour éviter les cookies',
 	),
 );
